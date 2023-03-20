72bddbd6bb7e1036e0472d921047358b3eb40152
Charles Albert <charles.ccomp@gmail.com>
2023-03-20 14:25:34 -0300

    correção do diagrama de classe e das classes das entidades do sistema

--- models/Premio.js
+++ models/Premio.js
@@ -6,23 +6,23 @@ const PremioSchema = new mongoose.Schema({
         required: true,
         type: String
     },
-    cronograma: {
-        required: true,
-        type: [Etapa]
-    },
     descricao: {
         required: true,
         type: String
+    },
+    ano: {
+        required: true,
+        type: String
+    },
+    cronograma: {
+        required: true,
+        type: [Etapa]
     },
     dataInicio: {
         required: true,
         type: String
     },
     dataFim: {
-        required: true,
-        type: String
-    },
-    ano: {
         required: true,
         type: String
     }
