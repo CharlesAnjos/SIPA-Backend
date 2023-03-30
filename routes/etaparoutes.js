9a32ad9c1e55a2d9117038d9759d81d690c3ad49
Charles Albert <charles.ccomp@gmail.com>
2023-03-30 20:26:59 -0300

    correções pontuais

--- routes/etaparoutes.js
+++ routes/etaparoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Etapa = require("../models/Etapa");
-const Premio = require("../models/Premio");
 
 module.exports = router;
 
