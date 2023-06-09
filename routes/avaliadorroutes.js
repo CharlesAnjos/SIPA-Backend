0612399312c546e834c9c875cccc58ec75a3e820
Charles Albert <charles.ccomp@gmail.com>
2023-06-09 12:03:41 -0300

    endpoints de avaliador alterados para melhor atender as requisições

--- routes/avaliadorroutes.js
+++ routes/avaliadorroutes.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Avaliador = require("../models/Avaliador");
+const Pessoa = require("../models/Pessoa");
+const mongoose = require('mongoose');
 
 module.exports = router;
 
 router.post('/novo', async(req, res) => {
-    const data = new Avaliador({
-        pessoa: req.body.pessoa,
-        registro: req.body.registro,
-        area: req.body.area
+    const pessoa = new Pessoa({
+        nome: req.body.pessoa.nome,
+        cpf: req.body.pessoa.cpf,
+        email: req.body.pessoa.email
     });
-
     try{
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
+        const pessoaToSave = await pessoa.save();
+        const data = new Avaliador({
+            pessoa: pessoaToSave,
+            registro: req.body.registro,
+            area: req.body.area
+        });
+        try{
+            const dataToSave = await data.save();
+            res.status(200).json(dataToSave)
+        }
+        catch(error){
+            res.status(400).json({message: error.message})
+        }
     }
     catch(error){
         res.status(400).json({message: error.message})
@@ -22,15 +34,8 @@ router.post('/novo', async(req, res) => {
 
 router.get('/listar', async (req, res) => {
     try {
-        var avaliadores = "";
-        const data = await Avaliador.find();
-        data.forEach(function(avaliador) {
-            avaliadores+=`\n Avaliador: ${avaliador.pessoa} (${avaliador._id})`;
-            avaliadores+=`\n     Registro: ${avaliador.registro}`;
-            avaliadores+=`\n     Área: ${avaliador.area}`;
-            avaliadores+=`\n`;
-        });
-        res.send(avaliadores);
+        const data = await getAvaliadoresPessoas();
+        res.send(data);
     }
     catch (error) {
         res.status(400).json({message: error.message})
@@ -39,13 +44,9 @@ router.get('/listar', async (req, res) => {
 
 router.get('/consultar/:id', async (req, res) => {
     try {
-        var avaliador = "";
         const id = req.params.id;
-        const data = await Avaliador.findById(id);
-        avaliador+=`\n Avaliador: ${data.pessoa} (${data._id})`;
-        avaliador+=`\n    Registro: ${data.registro}`;
-        avaliador+=`\n    Área: ${data.area}`;
-        res.send(avaliador);
+        const data = await getAvaliadorPessoa(id);
+        res.send(data);
     }
     catch (error) {
         res.status(400).json({message: error.message})
@@ -53,28 +54,88 @@ router.get('/consultar/:id', async (req, res) => {
 });
 
 router.patch('/atualizar/:id', async (req, res) => {
-
     try{
-        const id = req.params.id;
-        const data = await Avaliador.findByIdAndUpdate(id, {
-            pessoa: req.body.pessoa,
-            registro: req.body.registro,
-            area: req.body.area
-        })
-        res.send(`Avaliador ${req.body.pessoa} atualizado!`);
+        const aid = req.params.id;
+        const pid = req.body.pessoa.id;
+        const pessoaData = await Pessoa.findByIdAndUpdate(pid, {
+            nome: req.body.pessoa.nome,
+            cpf: req.body.pessoa.cpf,
+            email: req.body.pessoa.email
+        });
+        try{
+            const avaliadorData = await Avaliador.findByIdAndUpdate(aid, {
+                pessoa: pessoaData,
+                registro: req.body.registro,
+                area: req.body.area
+            });
+            res.send(avaliadorData);
+        }
+        catch(error){
+            res.status(400).json({message: error.message});
+        }
     }
     catch(error){
-        res.status(400).json({message: error.message})
+        res.status(400).json({message: error.message});
     }
 });
 
 router.delete('/remover/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        const data = await Avaliador.findByIdAndDelete(id);
-        res.send(`Avaliador ${data.pessoa} foi removido!`);
+        const avaliadorData = await Avaliador.findByIdAndDelete(id);
+        const pessoaData = await Pessoa.findByIdAndDelete(avaliadorData.pessoa._id);
+        res.send(`avaliador removido:\n${avaliadorData}`);
     }
     catch (error) {
         res.status(400).json({message: error.message})
     }
 });
+
+async function getAvaliadoresPessoas(){
+    var avaliadores = null;
+    try {
+        avaliadores = await Avaliador.aggregate([
+            {
+                $lookup: {
+                    from: Pessoa.collection.name,
+                    localField: 'pessoa',
+                    foreignField: '_id',
+                    as: 'pessoa'
+                },
+            },
+            {
+                $unwind: "$pessoa"
+            }
+        ]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    return avaliadores;
+};
+
+async function getAvaliadorPessoa(id){
+    var avaliador = null;
+    try {
+        avaliador = await Avaliador.aggregate([
+            {
+                $match: { "_id": mongoose.Types.ObjectId(id)},
+            },
+            {
+                $lookup: {
+                    from: Pessoa.collection.name,
+                    localField: 'pessoa',
+                    foreignField: '_id',
+                    as: 'pessoa'
+                },
+            },
+            {
+                $unwind: "$pessoa"
+            }
+        ]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    return avaliador;
+};
